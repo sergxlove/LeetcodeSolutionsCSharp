1f36ad0a09e74d62ff460c5c1358ad40d503fa3a
sergxlove <sergxlove@mail.ru>
2025-07-01 21:23:13 +0600

    add task 0020

---
+++ Solutions/0020_ValidParentheses.cs
@@ -0,0 +1,52 @@
+﻿namespace Solutions
+{
+    public class Solution0020
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> parentheses = new Stack<char>();
+            Dictionary<char, bool> isExitsParentheses = new Dictionary<char, bool>()
+            {
+                { '(', false },
+                { ')', true },
+                { '[', false },
+                { ']', true },
+                { '{', false },
+                { '}', true }
+            };
+            Dictionary<char, char> varParentheses = new Dictionary<char, char>()
+            {
+                { ')', '(' },
+                { ']', '[' },
+                { '}', '{' }
+            };
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!isExitsParentheses[s[i]])
+                {
+                    parentheses.Push(s[i]);
+                }
+                else
+                {
+                    try
+                    {
+                        if (parentheses.Peek() == varParentheses[s[i]])
+                        {
+                            parentheses.Pop();
+                        }
+                        else
+                        {
+                            parentheses.Push(s[i]);
+                        }
+                    }
+                    catch 
+                    {
+                        parentheses.Push(s[i]);
+                    }
+                }
+            }
+            if (parentheses.Count == 0) return true;
+            return false;
+        }
+    }
+}
