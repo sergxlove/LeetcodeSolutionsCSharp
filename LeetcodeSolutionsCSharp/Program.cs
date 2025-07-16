27cab4a5c159379fba0ee8676987e0ed5579ba06
sergxlove <sergxlove@mail.ru>
2025-07-16 22:09:40 +0600

    add task 0037

--- LeetcodeSolutionsCSharp/Program.cs
+++ LeetcodeSolutionsCSharp/Program.cs
@@ -6,10 +6,29 @@ namespace LeetcodeSolutionsCSharp
     {
         static void Main(string[] args)
         {
-            
+            char[][] board = new char[][] {
+            new char[] {'5','3','.','.','7','.','.','.','.'},
+            new char[] {'6','.','.','1','9','5','.','.','.'},
+            new char[] {'.','9','8','.','.','.','.','6','.'},
+            new char[] {'8','.','.','.','6','.','.','.','3'},
+            new char[] {'4','.','.','8','.','3','.','.','1'},
+            new char[] {'7','.','.','.','2','.','.','.','6'},
+            new char[] {'.','6','.','.','.','.','2','8','.'},
+            new char[] {'.','.','.','4','1','9','.','.','5'},
+            new char[] {'.','.','.','.','8','.','.','7','9'}
+        };
 
-            Solution0048 s = new Solution0048();
-            s.Rotate([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+            Solution0037 s = new Solution0037();
+            s.SolveSudoku(board);
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(board[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ Solutions/0037_SudokuSolver.cs
@@ -0,0 +1,59 @@
+﻿namespace Solutions
+{
+    public class Solution0037
+    {
+        public void SolveSudoku(char[][] board)
+        {
+            Solve(board);
+        }
+        public bool Solve(char[][] board)
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    if (board[row][col] == '.')
+                    {
+                        for (char num = '1'; num <= '9'; num++)
+                        {
+                            if (IsValid(board, row, col, num))
+                            {
+                                board[row][col] = num;
+
+                                if (Solve(board))
+                                    return true;
+                                else
+                                    board[row][col] = '.';
+                            }
+                        }
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private bool IsValid(char[][] board, int row, int col, char num)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (board[row][i] == num) return false;
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (board[i][col] == num) return false;
+            }
+            int boxRow = row - row % 3;
+            int boxCol = col - col % 3;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[boxRow + i][boxCol + j] == num)
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
