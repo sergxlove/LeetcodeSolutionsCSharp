b914069b24a4805919595514b56ab50366cb3dd3
sergxlove <sergxlove@mail.ru>
2025-07-18 23:34:58 +0600

    add task 0026, 0054, 0153

---
+++ Solutions/0026_RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,21 @@
+﻿namespace Solutions
+{
+    public class Solution0026
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int k = 0;
+            int lastvalue = int.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(lastvalue != nums[i])
+                {
+                    lastvalue = nums[i];
+                    nums[k] = lastvalue;
+                    k++;
+                }
+            }
+            return k;
+        }
+    }
+}
---
+++ Solutions/0054_SpiralMatrix.cs
@@ -0,0 +1,72 @@
+﻿namespace Solutions
+{
+    public class Solution0054
+    {
+        public IList<int> SpiralOrder(int[][] matrix)
+        {
+            IList<int> result = new List<int>();
+            int rows = matrix.Length;
+            int cols = matrix[0].Length;
+            int x = 0;
+            int y = 0;
+            int count = 1;
+            Target move = Target.right;
+            while (count <= rows * cols)
+            {
+                result.Add(matrix[x][y]);
+                matrix[x][y] = int.MinValue;
+                count++;
+                switch(move)
+                {
+                    case Target.right:
+                        y++;
+                        if (y == cols || matrix[x][y] == int.MinValue)
+                        {
+                            x++;
+                            y--;
+                            move = Target.down;
+                        }
+                        break;
+                    case Target.down:
+                        x++;
+                        if (x == rows || matrix[x][y] == int.MinValue)
+                        {
+                            y--;
+                            x--;
+                            move = Target.left;
+                        }
+                        break;
+                    case Target.left:
+                        y--;
+                        if (y == -1 || matrix[x][y] == int.MinValue)
+                        {
+                            x--;
+                            y++;
+                            move = Target.up;
+                        }
+                        break;
+                    case Target.up:
+                        x--;
+                        if (x == -1 || matrix[x][y] == int.MinValue)
+                        {
+                            y++;
+                            x++;
+                            move = Target.right;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return result;
+        }
+
+        enum Target
+        {
+            up,
+            down,
+            left,
+            right
+        }
+    }
+}
