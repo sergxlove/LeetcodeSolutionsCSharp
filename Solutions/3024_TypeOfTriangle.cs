2c7da7234fc42e38ffdd3e1c71c1bcfb3acdeeb5
sergxlove <sergxlove@mail.ru>
2025-07-20 21:21:31 +0600

    add task 3024

---
+++ Solutions/3024_TypeOfTriangle.cs
@@ -0,0 +1,13 @@
+﻿namespace Solutions
+{
+    public class Solution3024
+    {
+        public string TriangleType(int[] nums)
+        {
+            if (nums[0] + nums[1] <= nums[2] || nums[0] + nums[2] <= nums[1] || nums[1] + nums[2] <= nums[0]) return "none"; 
+            if (nums[0] == nums[1] && nums[1] == nums[2]) return "equilateral";
+            if (nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2]) return "isosceles";
+            return "scalene";
+        }
+    }
+}
