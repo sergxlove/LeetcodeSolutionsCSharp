b3acab368f3204434220316e914984a24a2f2be8
sergxlove <sergxlove@mail.ru>
2025-07-22 13:09:39 +0600

    add task 1695, 1957

---
+++ Solutions/1957_DeleteCharactersToMakeFancyString.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Solutions
+{
+    public class Solution1957
+    {
+        public string MakeFancyString(string s)
+        {
+            StringBuilder result = new StringBuilder();
+            int count = 1;
+            char currentChar = s[0];
+            result.Append(currentChar);
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (currentChar != s[i])
+                {
+                    count = 1;
+                    currentChar = s[i];
+                    result.Append(s[i]);
+                    continue;
+                }
+                if(count == 2) continue;
+                count++;
+                result.Append(s[i]);
+            }
+            return result.ToString();
+        }
+    }
+}
