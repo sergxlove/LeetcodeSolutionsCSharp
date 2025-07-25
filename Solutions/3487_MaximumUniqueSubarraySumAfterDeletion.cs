3d7c8b4c89e8dca42935bedcd1b1f49851e8b065
sergxlove <sergxlove@mail.ru>
2025-07-25 12:01:04 +0600

    add task 3487

---
+++ Solutions/3487_MaximumUniqueSubarraySumAfterDeletion.cs
@@ -0,0 +1,22 @@
+﻿namespace Solutions
+{
+    public class Solution3487
+    {
+        public int MaxSum(int[] nums)
+        {
+            HashSet<int> uniqueValue = new HashSet<int>();
+            int result = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] <= 0) continue;
+                if(!uniqueValue.Contains(nums[i])) 
+                {
+                    uniqueValue.Add(nums[i]);
+                    result += nums[i];
+                }
+            }
+            if (uniqueValue.Count == 0) result = nums.Max();
+            return result;
+        }
+    }
+}
