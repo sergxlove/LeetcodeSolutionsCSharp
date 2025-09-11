96241e4542779fd4ee3cda8c1b381c1b5d8c038b
sergxlove <sergxlove@mail.ru>
2025-09-11 20:39:53 +0600

    add task 2785

---
+++ Solutions/2785_SortVowelsInAString.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace Solutions
+{
+    public class Solution2785
+    {
+        public string SortVowels(string s)
+        {
+            List<char> needChar = new List<char>()
+            {
+                'a', 'e', 'i', 'o', 'u',
+                'A', 'E', 'I', 'O', 'U'
+            };
+            List<char> currentChar = new List<char>();
+            List<char> currentS = new List<char>();
+            List<int> positionChar = new List<int>();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (needChar.Contains(s[i]))
+                {
+                    currentChar.Add(s[i]);
+                    positionChar.Add(i);
+                    currentS.Add('*');
+                    continue;
+                }
+                currentS.Add(s[i]);
+            }
+            currentChar.Sort();
+            for (int i = 0; i < positionChar.Count; i++)
+            {
+                currentS[positionChar[i]] = currentChar[i];
+            }
+            for (int i = 0; i < currentS.Count; i++)
+            {
+                sb.Append(currentS[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
