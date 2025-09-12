c8f343120d7ce27789a172a57ce97359b8bd8b89
sergxlove <sergxlove@mail.ru>
2025-09-12 22:51:38 +0600

    add task 58

---
+++ Solutions/0058_LengthOfLastWord.cs
@@ -0,0 +1,12 @@
+﻿namespace Solutions
+{
+    public class Solution0058
+    {
+        public int LengthOfLastWord(string s)
+        {
+            s = s.Trim();
+            var words = s.Split(' ');
+            return words[words.Length - 1].Length;
+        }
+    }
+}
