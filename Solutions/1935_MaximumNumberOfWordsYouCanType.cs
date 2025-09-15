1c43353a1cca25dae64da6c7ebda7707096d26c7
sergxlove <sergxlove@mail.ru>
2025-09-15 14:27:45 +0600

    add task 1935

---
+++ Solutions/1935_MaximumNumberOfWordsYouCanType.cs
@@ -0,0 +1,26 @@
+﻿namespace Solutions
+{
+    public class Solution1935
+    {
+        public int CanBeTypedWords(string text, string brokenLetters)
+        {
+            var words = text.Split(' ');
+            int result = 0;
+            bool isValid = false;
+            for (int i = 0; i < words.Length; i++)
+            {
+                isValid = true;
+                for (int j = 0; j < brokenLetters.Length; j++)
+                {
+                    if (words[i].Contains(brokenLetters[j]))
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+                if (isValid) result++;
+            }
+            return result;
+        }
+    }
+}
