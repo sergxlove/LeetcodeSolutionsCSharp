82fefdb23cf74d6baed9b5e949b1426a1e395709
sergxlove <sergxlove@mail.ru>
2025-09-25 19:30:43 +0600

    add task 0088 and 0125

---
+++ Solutions/0088_MergeSortedArray.cs
@@ -0,0 +1,20 @@
+﻿namespace Solutions
+{
+    public class Solution0088
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            List<int> nums = new List<int>(n + m);
+            for(int i = 0; i < m; i++)
+            {
+                nums.Add(nums1[i]);
+            }
+            nums.AddRange(nums2);
+            nums.Sort();
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                nums1[i] = nums[i];
+            }
+        }
+    }
+}
