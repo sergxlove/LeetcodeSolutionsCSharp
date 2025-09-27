64b77ea167bfc1324c076e2730fc13da0cf58fb1
sergxlove <sergxlove@mail.ru>
2025-09-27 13:12:22 +0600

    add task 0812, 0976

---
+++ Solutions/0976_LargestPerimeterTriangle.cs
@@ -0,0 +1,22 @@
+﻿namespace Solutions
+{
+    public class Solution0976
+    {
+        public int LargestPerimeter(int[] nums)
+        {
+            Array.Sort(nums);
+            Array.Reverse(nums);
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                int a = nums[i];
+                int b = nums[i + 1];
+                int c = nums[i + 2];
+                if (b + c > a)
+                {
+                    return a + b + c;
+                }
+            }
+            return 0;
+        }
+    }
+}
