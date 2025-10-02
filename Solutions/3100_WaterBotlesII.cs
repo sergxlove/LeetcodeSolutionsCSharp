88a1b867eb2b442cde3e0f61379ca6febeed315c
sergxlove <sergxlove@mail.ru>
2025-10-03 00:00:44 +0600

    add task 3100

---
+++ Solutions/3100_WaterBotlesII.cs
@@ -0,0 +1,19 @@
+﻿namespace Solutions
+{
+    public class Solution3100
+    {
+        public int MaxBottlesDrunk(int numBottles, int numExchange)
+        {
+            int result = numBottles;
+            int empty = numBottles;
+            while(empty >= numExchange)
+            {
+                empty -= numExchange;
+                result++;
+                empty++;
+                numExchange++;
+            }
+            return result;
+        }
+    }
+}
