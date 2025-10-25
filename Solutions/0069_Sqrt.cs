e26f3775e22504017aa10b8affd3f9d6c54cb7ac
sergxlove <sergxlove@mail.ru>
2025-10-25 22:15:15 +0600

    add task 0069

---
+++ Solutions/0069_Sqrt.cs
@@ -0,0 +1,20 @@
+﻿namespace Solutions
+{
+    public class Solution0069
+    {
+        public int MySqrt(int x)
+        {
+            if (x == 0 || x == 1) return x;
+            double precision = 1e-15;
+            double newX = x;
+            double root;
+            while(true)
+            {
+                root = 0.5 * (newX + (x / newX));
+                if (Math.Abs(root - newX) < precision) break;
+                newX = root;
+            }
+            return Convert.ToInt32(Math.Truncate(root));
+        }
+    }
+}
