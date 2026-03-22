6afc5bdc86e13351e0a1d57c2c3248a5727fabd0
sergxlove <sergxlove@mail.ru>
2026-03-22 16:38:48 +0600

    add task 0099,  0100, 1886

---
+++ Solutions/0099_RecoverBinarySearchTree.cs
@@ -0,0 +1,51 @@
+﻿namespace Solutions
+{
+    public class Solution0099
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        private TreeNode first;   
+        private TreeNode second; 
+        private TreeNode prev; 
+
+        public void RecoverTree(TreeNode root)
+        {
+            first = null;
+            second = null;
+            prev = null;
+            InorderTraversal(root);
+            if (first != null && second != null)
+            {
+                int temp = first.val;
+                first.val = second.val;
+                second.val = temp;
+            }
+        }
+
+        private void InorderTraversal(TreeNode node)
+        {
+            if (node == null) return;
+            InorderTraversal(node.left);
+            if (prev != null && prev.val > node.val)
+            {
+                if (first == null)
+                {
+                    first = prev;
+                }
+                second = node;
+            }
+            prev = node;
+            InorderTraversal(node.right);
+        }
+    }
+}
---
+++ Solutions/1886_DetermineWhetherMatrixCanBeObtainedByRotation.cs
@@ -0,0 +1,51 @@
+﻿namespace Solutions
+{
+    public class Solution1886
+    {
+        public bool FindRotation(int[][] mat, int[][] target)
+        {
+            int n = mat.Length;
+            for (int rotation = 0; rotation < 4; rotation++)
+            {
+                if (AreMatricesEqual(mat, target, n))
+                {
+                    return true;
+                }
+                mat = Rotate90(mat, n);
+            }
+            return false;
+        }
+
+        private bool AreMatricesEqual(int[][] mat, int[][] target, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i][j] != target[i][j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private int[][] Rotate90(int[][] matrix, int n)
+        {
+            int[][] rotated = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                rotated[i] = new int[n];
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    rotated[j][n - 1 - i] = matrix[i][j];
+                }
+            }
+            return rotated;
+        }
+    }
+}
