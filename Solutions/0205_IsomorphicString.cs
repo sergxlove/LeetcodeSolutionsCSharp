973178a1fbe6330a162581e8091ffc7538036b49
sergxlove <sergxlove@mail.ru>
2026-03-31 18:24:41 +0600

    add task 0169, 0205, 2404

---
+++ Solutions/0205_IsomorphicString.cs
@@ -0,0 +1,34 @@
+﻿namespace Solutions
+{
+    public class Solution0205
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+            Dictionary<char, char> mapST = new Dictionary<char, char>();
+            Dictionary<char, char> mapTS = new Dictionary<char, char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char charS = s[i];
+                char charT = t[i];
+                if (mapST.ContainsKey(charS))
+                {
+                    if (mapST[charS] != charT) return false;
+                }
+                else
+                {
+                    mapST[charS] = charT;
+                }
+                if (mapTS.ContainsKey(charT))
+                {
+                    if (mapTS[charT] != charS) return false;
+                }
+                else
+                {
+                    mapTS[charT] = charS;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Solutions/2404_MostFrequentEvenElement.cs
@@ -0,0 +1,28 @@
+﻿namespace Solutions
+{
+    public class Solution2404
+    {
+        public int MostFrequentEven(int[] nums)
+        {
+            Dictionary<int, int> frequencyMap = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                if (num % 2 != 0) continue;
+                if (frequencyMap.ContainsKey(num)) frequencyMap[num]++;
+                else frequencyMap[num] = 1;
+            }
+            if (frequencyMap.Count == 0) return -1;
+            int mostFrequent = -1;
+            int maxCount = 0;
+            foreach (var pair in frequencyMap)
+            {
+                if (pair.Value > maxCount || (pair.Value == maxCount && pair.Key < mostFrequent))
+                {
+                    mostFrequent = pair.Key;
+                    maxCount = pair.Value;
+                }
+            }
+            return mostFrequent;
+        }
+    }
+}
