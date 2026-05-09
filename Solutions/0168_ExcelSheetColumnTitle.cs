2fc431e0c53087ec366c1d1dfb721f74b8eacf2f
sergxlove <sergxlove@mail.ru>
2026-05-09 20:41:26 +0600

    add task 168

---
+++ Solutions/0168_ExcelSheetColumnTitle.cs
@@ -0,0 +1,18 @@
+﻿namespace Solutions
+{
+    public class Solution0168
+    {
+        public string ConvertToTitle(int columnNumber)
+        {
+            string result = string.Empty;
+            while (columnNumber > 0)
+            {
+                columnNumber--;
+                char letter = (char)('A' + columnNumber % 26);
+                result = letter + result;
+                columnNumber /= 26;
+            }
+            return result;
+        }
+    }
+}
