094f92597a4ba519e62b6bd8ce9cfde701bdd635
sergxlove <sergxlove@mail.ru>
2026-05-29 15:43:20 +0600

    add task 3300

---
+++ Solutions/3300_MinimumElementAfterReplacementWithDigitSum.cs
@@ -0,0 +1,22 @@
+﻿namespace Solutions
+{
+    public class Solution3300
+    {
+        public int MinElement(int[] nums)
+        {
+            int min = int.MaxValue;
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                string valueStr = Convert.ToString(nums[i]);
+                for (int j = 0; j < valueStr.Length; j++)
+                {
+                    sum += valueStr[j] - '0';
+                }
+                if (sum < min) min = sum;
+                sum = 0;
+            }
+            return min;
+        }
+    }
+}
